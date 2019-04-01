583bbe38a17da1036c518d28c9b6160b397f6f35
Doan Hoang Vu <doanhuuanh4@gmail.com>
2019-04-01 19:57:16 +0700

    Chapter 14: Add user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -7,6 +7,11 @@ class Micropost < ApplicationRecord
   validates :content, presence: true, length: {maximum:
     Settings.micropost.content.maximum}
   validate  :picture_size
+  scope :load_user_followed, (lambda do |id|
+    following_ids = "SELECT followed_id FROM relationships
+      WHERE  follower_id = :user_id"
+    where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
+  end)
 
   private
 
